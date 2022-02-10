ddd8ce2a7911baac88fb5a248c23e0e4c3deee0b
Gabriel Valle <you@example.comgabrielvallerodrigues@gmail.com>
2022-02-10 12:47:39 -0300

    Finalizada aula 2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import HelloWorld from './components/HelloWorld';
-import SayMyName from './components/SayMyName';
-import Pessoa from './components/Pessoa';
-import Frases from './components/Frases';
-import List from './components/List';
 import Evento from './components/Evento';
 import Form from './components/Form';
+import Condicional from './components/Condicional';
+import OutraLista from './components/OutraLista';
+
+
 
 function App() {
-  var name = "Laysa"
- 
+
+  //reinderização de listas
+
+  const meusItens = ['React', 'veu', 'Angular']
+   
   return (
     <div className="App">
-      <h1>Agora deu certo</h1>
-      <HelloWorld/>
-      <SayMyName name="Gabriel"/>
-      <SayMyName name= {name}/>
-      <Pessoa nome="Gabriel" idade="28" profissao="Programador"  foto="https://static.wikia.nocookie.net/swordartonline/images/e/e2/Kirito_VR.png/revision/latest?cb=20210905161320&path-prefix=pt-br"/>
-      <Frases/>
-      <List/>
-      <Evento/>
-      <Form/>
+      <h1>Reinderização Condicional</h1>
+      <Condicional/>
+      <h1>Reinderização de listas</h1>
+      <OutraLista itens={meusItens} />
     </div>
   );
 }
